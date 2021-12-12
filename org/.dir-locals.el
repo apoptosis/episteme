30598497ae11fbf99e4ee69266ca889eafb13dc5
Lord_Valen <lord_valen@protonmail.com>
2021-12-11 22:03:55 -0500

    Begin working on nix cards, fix dir-locals

--- org/.dir-locals.el
+++ org/.dir-locals.el
@@ -1,1 +1,2 @@
-((nil . ((eval . (setq org-roam-directory (expand-file-name "./"))))))
+((nil . ((eval . (setq-local org-roam-directory (expand-file-name (locate-dominating-file default-directory ".dir-locals.el"))))
+         (eval . (setq-local org-roam-db-location (expand-file-name (concat org-roam-directory "org-roam.db")))))))
