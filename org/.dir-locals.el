ad9e423dd124b061e7e0dca04da1e52fc3c37cc8
Lord-Valen <lord_valen@protonmail.com>
2021-12-12 13:32:42 -0500

    Settle on roam management solution

--- org/.dir-locals.el
+++
@@ -1,2 +0,0 @@
-((nil . ((eval . (setq-local org-roam-directory (expand-file-name (locate-dominating-file default-directory ".dir-locals.el"))))
-         (eval . (setq-local org-roam-db-location (expand-file-name (concat org-roam-directory "org-roam.db")))))))
