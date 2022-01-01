34a10a63f835cdda880cd76d07619b0c6d55f9e9
Dustin Lacewell <dlacewell@gmail.com>
2021-12-31 18:08:52 -0600

    Add flake.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> {};
+{ runCommand }:
 
 runCommand "episteme" {} ''
   mkdir -p $out/bin
---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+       defaultPackage.x86_64-linux = pkgs.callPackage ./default.nix {};
+    };
+}
