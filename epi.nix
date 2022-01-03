18c2e6ef79b51bed7ae69034db8190338ccfb983
Dustin Lacewell <dlacewell@gmail.com>
2022-01-03 04:50:06 -0600

    Remove old nix helper

--- epi.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs> {};
-
-runCommand "epi" {} ''
-  mkdir -p $out/bin
-  echo "#!/usr/bin/env bash
-  episteme -repo ~/src/apoptosis/episteme" > $out/bin/epi
-  chmod +x $out/bin/epi
-''
