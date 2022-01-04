0f219f948ba8c4fd38097149c4fbcb9adaeb756c
Dustin Lacewell <dlacewell@gmail.com>
2022-01-03 21:02:20 -0600

    Update cli scripts

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
-{ runCommand }:
+{ pkgs ? import <nixpkgs> {} }:
 
-runCommand "episteme" {} ''
+pkgs.runCommand "episteme" {} ''
   mkdir -p $out/bin
   cp ${./episteme} $out/bin/episteme
   chmod +x $out/bin/episteme
